bc10f122821f44867063ed6519cb904243b630be
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-03-01 13:33:17 +0300

    feat: change cycle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?'); //создаем вопрс в промте
 
-const personalMovieDB = {
+const personalMovieDB = {  // объект база данных
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,13 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-let a = prompt('Один из последних проссмотренных фильов?'),
-    b = prompt('На сколько вы его оцениваете?'),
-    c = prompt('Один из последних проссмотренных фильов?'),
-    d = prompt('На сколько вы его оцениваете?');
 
-personalMovieDB.movies[a] = b;  // записываем данные в объект(присваиваем ключу а значение b)
-personalMovieDB.movies[c] = d;  
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних проссмотренных фильов?'),
+        b = prompt('На сколько вы его оцениваете?');
 
+    if (a != null && b != null && a != '' && b != '' && a.length <50) { // проверяем на соответсвие условиям
+        personalMovieDB.movies[a] = b;  // если условия выполнены записываем данные в объект(присваиваем ключу а значение b)
+    } else {
+        i--  // уходим на 1 цикл назад
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log ('Просмотренно довольно мало фильмов')
+} else if (10 <= personalMovieDB.count <= 30) {
+    console.log ('Вы классический зритель')
+} else if (personalMovieDB > 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
 
 console.log(personalMovieDB)
+
+
+
+
